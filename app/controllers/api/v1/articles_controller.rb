04c6a36842badab0da0027da9877b6a809e43300
Max Wong <max@techsamurais.com>
2020-01-20 16:15:23 -0800

    build post route

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -19,4 +19,29 @@ class Api::V1::ArticlesController < ApplicationController
     status: :ok
   end
   
+  def create
+    article = Article.new(article_params)
+
+    if article.save
+      render json: {
+      status: 'SUCCESS',
+      message: 'Saving single article',
+      data: article
+      },
+      status: :ok
+    else 
+      render json: {
+        status: 'ERROR',
+        message: 'Article not saved',
+        data: data.article.errors
+      },
+      status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :body)
+  end
 end
